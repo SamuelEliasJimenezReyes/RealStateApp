693967a4f3732fba3ce00e5aa633947958bf6007
Domingojr21 <129601835+Domingojr21@users.noreply.github.com>
2023-12-14 13:54:46 -0400

    propiedades al 100%

--- RealState.Infraestructure.Persistence/Repositories/PropertyRepository.cs
+++ RealState.Infraestructure.Persistence/Repositories/PropertyRepository.cs
@@ -7,8 +7,18 @@ namespace RealState.Infraestructure.Persistence.Repositories
 
     public class PropertiesRepository : GenericRepository<Properties>, IPropertiesRepository
     {
+        private readonly RealStateContext _context;
+
         public PropertiesRepository(RealStateContext dbContext) : base(dbContext)
         {
+            _context = dbContext;
+        }
+
+        public override async Task DeleteAsync(Properties entity)
+        {
+            entity.IsDeleted = true;
+            await UpdateAsync(entity, entity.Id);
+            await _context.SaveChangesAsync();
         }
     }
 
--- RealStateApp/Controllers/PropertiesController.cs
+++ RealStateApp/Controllers/PropertiesController.cs
@@ -92,5 +92,98 @@ namespace RealStateApp.Controllers
             return View(svm);
 
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var savePropertiesVM = new SavePropertiesVM();
+            savePropertiesVM.PropertiesTypes = await _propertiesTypesService.GetAllViewModel();
+            savePropertiesVM.SalesTypes = await _salesTypeService.GetAllViewModel();
+            savePropertiesVM.Improvements = await _ImprovementsService.GetAllViewModel();
+
+            var list = await _propertiesService.GetAllPropertiesVM(new PropertiesFilterVM());
+           var property = list.FirstOrDefault(x => x.Id ==  id);
+            var pForRelationshipsId = await _propertiesService.GetByIdSaveViewModel(property.Id);
+
+            savePropertiesVM.Price = property.Price;
+            savePropertiesVM.Id = property.Id;
+            savePropertiesVM.LandSizeMeter = property.LandSizeMeter;
+            savePropertiesVM.BathroomQuantity = property.BathroomQuantity;
+            savePropertiesVM.RoomQuantity = property.RoomQuantity;
+            savePropertiesVM.AgentId = property.Agent.UserId;
+            savePropertiesVM.Code = property.Code;
+            savePropertiesVM.Description = property.Description;
+            savePropertiesVM.SaleTypeId = pForRelationshipsId.SaleTypeId;
+            savePropertiesVM.Code = property.Code;
+            savePropertiesVM.PropertiesTypeId = pForRelationshipsId.PropertiesTypeId;
+
+            savePropertiesVM.PropertiesImprovementsId = property.PropertiesImprovements.Select(x => x.Id).ToList();
+            savePropertiesVM.ImagesProperties = property.ImagesProperties;
+                
+                return View(savePropertiesVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(SavePropertiesVM svm)
+        {
+            var imagesNeeditList = await _imagesPropertiesService.GetAllImagesByPropertyId(svm.Id);
+            int imagesNeedIt = imagesNeeditList.Count;
+            if (!ModelState.IsValid)
+            {
+                if (svm.File !=null && svm.File.Count > imagesNeedIt)
+                {
+                    svm.HasError = true;
+                    svm.ErrorMessage = $"Solo Puede seleccionar {imagesNeedIt} Imagenes, {svm.File.Count} exceden el número máximo de Imagenes";
+                    svm.PropertiesTypes = await _propertiesTypesService.GetAllViewModel();
+                    svm.SalesTypes = await _salesTypeService.GetAllViewModel();
+                    svm.Improvements = await _ImprovementsService.GetAllViewModel();
+                    svm.ImagesProperties = await _imagesPropertiesService.GetAllImagesByPropertyId(svm.Id);
+                    return View(svm);
+                }
+                
+                await _propertiesService.Update(svm, svm.Id);
+
+                if (svm.File != null && svm.File.Count >= 1)
+                {
+                    foreach (var file in svm.File)
+                    {
+                        var imagesgenerated = FileManager.Upload(file, svm.Code, "Properties");
+
+                        svm.ImagesProperties.Add(imagesgenerated);
+                    }
+
+                    foreach (var imagespath in svm.ImagesProperties)
+                    {
+                        var ip = new SaveImagesPropertiesVM
+                        {
+                            PropertiesId = svm.Id,
+                            ImageUrl = imagespath
+                        };
+
+                        await _imagesPropertiesService.Add(ip);
+                    }
+
+                }
+
+                return RedirectToRoute(new { controller = "Properties", action = "Index", agentId = svm.AgentId });
+
+            }
+
+            return View(svm);
+
+        }
+
+
+        public async Task<IActionResult> DeleteImages(int propertyId, string oldImagesPath)
+        {
+            await _imagesPropertiesService.DeleteImagesProperties(propertyId, oldImagesPath);
+            return RedirectToRoute(new { controller = "Properties", action = "Edit", id = propertyId });
+        }
+
+        public async Task<IActionResult> Delete(int propertyId, string agentId)
+        {
+            await _propertiesService.Delete(propertyId);
+             return RedirectToRoute(new { controller = "Properties", action = "Index", agentId = agentId });
+  
+        }
     }
 }
