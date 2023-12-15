9ec40f37205abef3369f8c28d28a25b24e2ecaf2
Domingojr21 <129601835+Domingojr21@users.noreply.github.com>
2023-12-14 20:04:25 -0400

    master vacana

--- RealStateApp.Core.Application/ViewModels/Improvents/SaveImproventsVM.cs
+++ RealStateApp.Core.Application/ViewModels/Improvents/SaveImproventsVM.cs
@@ -7,5 +7,6 @@ namespace RealStateApp.Core.Application.ViewModels.Improvements
         public int Id { get; set; }
         public string Description { get; set; }
         public string Name { get; set; }
+
     }
 }
