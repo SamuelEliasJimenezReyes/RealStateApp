09df1b59bcaaf9edf34a3cfca259d5dd211e1ff9
Domingojr21 <129601835+Domingojr21@users.noreply.github.com>
2023-12-14 20:03:40 -0400

    improvements ready al 200

--- RealStateApp.Core.Application/ViewModels/Improvents/SaveImproventsVM.cs
+++ RealStateApp.Core.Application/ViewModels/Improvents/SaveImproventsVM.cs
@@ -4,6 +4,7 @@ namespace RealStateApp.Core.Application.ViewModels.Improvements
 {
     public class SaveImprovementsVM
     {
+        public int Id { get; set; }
         public string Description { get; set; }
         public string Name { get; set; }
     }
